391eb8c9b673deba4ac5da582e8ec22a22c4944e
BalaMax <balacrz+max@gmail.com>
2021-05-13 13:20:03 +0530

    Updating routes

--- api/index.js
+++ api/index.js
@@ -1,9 +1,25 @@
-const app = require('express')()
 
-app.get('/api', (req, res) => {
-  res.setHeader('Content-Type', 'text/html')
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
-  res.end(`Hello! Go to item: `)
-})
+// server.js
 
+// BASE SETUP
+// ==============================================
+
+var express = require('express');
+var app     = express();
+var port    =   process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+const routes = require('../routes');
+
+//  Connect all our routes to our application
+app.use('/', routes);
+
+// START THE SERVER
+// ==============================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
 module.exports = app
