6d8f5adf83fcda0d60033c2f4fdbaa73f450a803
BalaMax <balacrz+max@gmail.com>
2021-05-13 13:24:08 +0530

    Updated index

--- api/index.js
+++ api/index.js
@@ -1,25 +1,16 @@
+const app = require('express')()
+const twilioProcess = require('../controllers/twilio')
 
-// server.js
+app.get('/api', (req, res) => {
+  res.setHeader('Content-Type', 'text/html')
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+  res.end(`Hello! Go to item: `)
+})
 
-// BASE SETUP
-// ==============================================
+app.get('/', function(req, res) {
+    res.send('Server is running and API is available to consume!');  
+});
 
-var express = require('express');
-var app     = express();
-var port    =   process.env.PORT || 8080;
+app.post('/api/sms', twilioProcess.sendSMS);
 
-app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
-
-const routes = require('../routes');
-
-//  Connect all our routes to our application
-app.use('/', routes);
-
-// START THE SERVER
-// ==============================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
 module.exports = app
