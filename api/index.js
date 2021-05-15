ea3016ba8b894cc232c59983d8008151e8456832
BalaMax <balacrz+max@gmail.com>
2021-05-15 07:32:57 +0530

    new api

--- api/index.js
+++ api/index.js
@@ -6,16 +6,13 @@ app.get('/api', (req, res) => {
   res.setHeader('Content-Type', 'text/html')
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
   res.end('Api services are running!')
-})
+});
 
 app.post('/api/mms', (req, res) => {
-    console.log(req.body);
-    const name = req?.body?.name ?? 'world';
-    console.log(name);
-    console.log(req?.body);
-    res.send(`Hello ${name}, you just parsed the req body!`)
+    const { name } = req.body;
+    res.send({status:'good', name});
   }
-  );
+);
 
 app.post('/api/sms', twilioProcess.sendSMS);
 
